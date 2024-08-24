9ab2f48c26b72592396e36504439379018504390
ZhannaKhizrieva <zhanna-apteka@mail.ru>
2024-08-24 18:15:02 +0300

    add useCallback() вот так делать не нужно!

--- my-app/src/app.js
+++ my-app/src/app.js
@@ -1,11 +1,40 @@
-import { ControlPanel, User } from './components';
+import { useState, useCallback } from 'react';
 import styles from './app.module.css';
 
 export const App = () => {
+	const [num, setNum] = useState(0);
+	const [degree, setDegree] = useState(0);
+	const [result, setResult] = useState(0);
+
+	const onNumChange = useCallback(
+		({ target }) => {
+			setNum(target.value);
+			setResult(Math.pow(target.value, degree));
+		},
+		[degree],
+	);
+
+	const onDegreeChange = useCallback(
+		({ target }) => {
+			setDegree(target.value);
+			setResult(Math.pow(num, target.value));
+		},
+		[num],
+	);
+
 	return (
 		<div className={styles.app}>
-			<User />
-			<ControlPanel />
+			<div>
+				{num} в степени {degree} = {result}
+			</div>
+			<label>
+				<span>Число: </span>
+				<input type="number" value={num} onChange={onNumChange} />
+			</label>
+			<label>
+				<span>Степень: </span>
+				<input type="number" value={degree} onChange={onDegreeChange} />
+			</label>
 		</div>
 	);
 };
